ae7f2c3027e820d206fa8f5d0a692fa5d73c86ac
M Farhan Hilal A K <127405443+frhhll@users.noreply.github.com>
2023-10-26 16:19:15 +0700

    Add files via upload

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
+#include <flutter_media_metadata/flutter_media_metadata_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterMediaMetadataPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterMediaMetadataPlugin"));
+}
