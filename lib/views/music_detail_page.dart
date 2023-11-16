1240db7edfb9a315292f87f02a31c9c9e1436cd7
M Farhan Hilal A K <127405443+frhhll@users.noreply.github.com>
2023-11-16 15:43:16 +0700

    Add files via upload

---
+++ lib/views/music_detail_page.dart
@@ -0,0 +1,65 @@
+//Now let's create the article details page
+
+import 'package:music_app/bindings/music_model.dart';
+import 'package:flutter/material.dart';
+
+class MusicsPage extends StatelessWidget {
+  final Musics musics;
+
+  const MusicsPage({super.key, required this.musics});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(musics.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 200.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                //let's add the height
+
+                image: DecorationImage(
+                    image: NetworkImage(musics.urlToImage), fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Container(
+              padding: const EdgeInsets.all(6.0),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Text(
+                musics.source.name,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              musics.description,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
